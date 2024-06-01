69513e4bf464d6e0548b99f1919617ed7c182e64
Kelly M <kelly.nicole.monson@gmail.com>
2024-06-01 20:42:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import { Tldraw } from 'tldraw'
+import './index.css'
+
+function App() {
+
+  return (
+    <div style={{ position: 'fixed', inset: 0 }}>
+        <Tldraw />
+    </div>
+  )
+}
+
+export default App
